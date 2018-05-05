95072e12fcbcf067a7dc203bdffc68df3d690493
AayushKunwar <Kunwar_aayush@live.com>
2018-05-05 13:08:53 +0545

    new

---
+++ Aayush first/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    Code Blocks program ma gareko
+    */
+
+    printf("Hello Sailendra Dai\n");
+    printf("Hello World");
+
+    return 0;
+}
